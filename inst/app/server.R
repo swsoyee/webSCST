3d2943aab50a8d8f842e24dbe5e9b2fe1bf38d72
InfinityLoop <swsoyee@gmail.com>
2021-10-30 15:29:54 +0900

    chore: rename file name

--- inst/app/server.R
+++ inst/app/server.R
@@ -1,4 +1,4 @@
 shinyServer(function(input, output, session) {
-  source(file = "./components/input-panel/upload-data-preview.server.R", local = TRUE)
+  source(file = "./components/input-panel/upload-data-preview-matrix.server.R", local = TRUE)
   source(file = "./components/input-panel/upload-data-preview-feature.server.R", local = TRUE)
 })
