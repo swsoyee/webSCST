ddcb09bbc8eb49e037cbf7ff6495f2122d5bf8e9
InfinityLoop <swsoyee@gmail.com>
2021-10-31 12:25:48 +0900

    style: minor layout

--- inst/app/pages/quality-control.R
+++ inst/app/pages/quality-control.R
@@ -1,55 +1,57 @@
 fluidPage(
-  box(
-    width = 12,
-    solidHeader = TRUE,
-    status = "primary",
-    title = tagList(
-      "Violin Plot and Feature Scatter"
-    ),
-    fluidRow(
-      column(
-        width = 3,
-        numericInputIcon(
-          inputId = "quality_control_min_gene",
-          label = "Minimum Gene",
-          value = 500,
-          step = 1
+  fluidRow(
+    box(
+      width = 12,
+      solidHeader = TRUE,
+      status = "primary",
+      title = tagList(
+        "Violin Plot and Feature Scatter"
+      ),
+      fluidRow(
+        column(
+          width = 3,
+          numericInputIcon(
+            inputId = "quality_control_min_gene",
+            label = "Minimum Gene",
+            value = 500,
+            step = 1
+          )
+        ),
+        column(
+          width = 3,
+          numericInputIcon(
+            inputId = "quality_control_max_gene",
+            label = "Maximum Gene",
+            value = 4000,
+            step = 1
+          )
+        ),
+        column(
+          width = 3,
+          numericInputIcon(
+            inputId = "quality_control_pctmt",
+            label = "Percent MT",
+            value = 15,
+            step = 1
+          )
+        ),
+        column(
+          width = 3,
+          tags$br(),
+          actionBttn(
+            inputId = "apply_quality_control_filter",
+            label = "Apply Filter",
+            color = "primary",
+            icon = icon("play"),
+            size = "sm",
+            style = "fill",
+            block = TRUE
+          )
         )
       ),
-      column(
-        width = 3,
-        numericInputIcon(
-          inputId = "quality_control_max_gene",
-          label = "Maximum Gene",
-          value = 4000,
-          step = 1
-        )
-      ),
-      column(
-        width = 3,
-        numericInputIcon(
-          inputId = "quality_control_pctmt",
-          label = "Percent MT",
-          value = 15,
-          step = 1
-        )
-      ),
-      column(
-        width = 3,
-        tags$br(),
-        actionBttn(
-          inputId = "apply_quality_control_filter",
-          label = "Apply Filter",
-          color = "primary",
-          icon = icon("play"),
-          size = "sm",
-          style = "fill",
-          block = TRUE
-        )
-      )
-    ),
-    textOutput("quality_control_plot_description"),
-    withSpinner(plotOutput("quality_control_violin_plot")),
-    withSpinner(plotOutput("quality_control_feature_scatter"))
+      textOutput("quality_control_plot_description"),
+      withSpinner(plotOutput("quality_control_violin_plot")),
+      withSpinner(plotOutput("quality_control_feature_scatter"))
+    )
   )
 )
