f1e35c22bc81b7a53e2fb23facb4761b77a6cbc7
InfinityLoop <swsoyee@gmail.com>
2021-11-23 16:39:07 +0900

    fix: output ssGSEA plot

--- inst/app/components/data-integration/ss-gsva.server.R
+++ inst/app/components/data-integration/ss-gsva.server.R
@@ -19,10 +19,24 @@ output$run_gsva <- renderUI({
 
 output$cell_type_selector_gsva <- renderUI({
   if (!is.null(global_data$marker) && global_data$gsva_done) {
+    all_col <- colnames(global_data$gsva_stRNA@meta.data)
+    not_choices <- c(
+      "orig.ident",
+      "nCount_RNA",
+      "nFeature_RNA",
+      "nCount_SCT",
+      "nFeature_SCT",
+      "SCT_snn_res.0.8",
+      "seurat_clusters",
+      "SCT_snn_res.0.3",
+      "SCT_snn_res.0.5"
+    )
+
+    choices <- all_col[!(all_col %in% not_choices)]
     pickerInput(
       inputId = "selected_cell_type_gsva",
       label = "Name of Cell Type",
-      choices = unique(as.character(global_data$marker$cluster))
+      choices = choices
     )
   }
 })
@@ -67,12 +81,12 @@ observeEvent(input$run_gsva, {
       top20.list,
       kcdf = "Poisson",
       method = "ssgsea",
-      abs.ranking = T, 
+      abs.ranking = T,
       parallel.sz = 3
     )
 
     es.matrix <- as.data.frame(t(es.matrix))
-    stRNA <- AddMetaData(stRNA, es.matrix) # TODO Warning: Invalid name supplied, making object name syntactically valid.
+    stRNA <- AddMetaData(stRNA, es.matrix)
 
     global_data$gsva_stRNA <- stRNA
     global_data$gsva_done <- TRUE
@@ -88,7 +102,7 @@ observeEvent(input$run_gsva, {
   }
 })
 
-observeEvent(input$cell_type_selector_gsva, {
+observeEvent(input$selected_cell_type_gsva, {
   output$selected_cell_type_gsva_featureplot <- renderPlot({
     stRNA <- global_data$gsva_stRNA
 
